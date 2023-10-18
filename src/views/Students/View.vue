374cd69af33fc949bcbde2d8f1f7e52edae2ea37
tecrodrigocastro <tec.rodrigocastro@gmail.com>
2023-10-18 16:27:51 -0300

    feat: install axios and get data api

--- src/views/Students/View.vue
+++ src/views/Students/View.vue
@@ -7,12 +7,47 @@ import { RouterLink } from 'vue-router';
         <div class="card-header">
             <h4>
                 Students
-                <RouterLink to="/student/create"> Add Student</RouterLink>
+                <RouterLink to="/student/create" class="btn btn-primary float-end"> Add Student</RouterLink>
             </h4>
+        </div>
+        <div class="card-body">
+            <table class="table table-bordered">
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Name</th>
+                        <th>Course</th>
+                        <th>Email</th>
+                        <th>Phone</th>
+                        <th>Created At</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr></tr>
+                </tbody>
+            </table>
         </div>
     </div>
 </template>
-
-<style>
-
-</style>
+<script>
+import axios from 'axios'
+export default {
+    name: 'students',
+    data() {
+        return {
+            students: [],
+        }
+    },
+    mounted() { 
+        this.getStudents();
+    },
+    methods: {
+        getStudents(){
+            axios.get('https://650496acc8869921ae2539d1.mockapi.io/api/students').then(res => {
+                console.log(res)
+            });
+        }
+    }
+}
+</script>
