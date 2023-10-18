842536129cde6785bea28a4bd1d38efb5906c67d
tecrodrigocastro <tec.rodrigocastro@gmail.com>
2023-10-18 16:14:57 -0300

    initial commit

---
+++ src/views/Students/View.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+</script>
+
+<template>
+    <div class="card">
+        <div class="card-header">
+            <h4>
+                Students
+                <RouterLink to="/student/create"> Add Student</RouterLink>
+            </h4>
+        </div>
+    </div>
+</template>
+
+<style>
+
+</style>
